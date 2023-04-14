5f4a1124c9f7014749e1ca66be7225b666ac379c
slapplebags <mtbales@fastmail.fm>
2023-04-14 10:57:16 -0700

    added dropdowns over autofs manager multi-selects and test code for the same on the backend. tested and working as expected.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from typing import Optional
+from typing import Optional, List
+from pydantic import BaseModel
 import secrets
 import subprocess
 import os
@@ -28,6 +29,9 @@ afsserverdict = {
 for i in afsserverdict:
     for x in afsserverdict[i]:
         afsserverlist.append(i + ":" + x)
+
+#simple get group list: sudo samba-tool group list
+#need to remove the built in groups
 
 @app.post("/result")
 def form_post(request: Request, username: str = Form(...), fname: str = Form(...), lname: str = Form(...),
@@ -59,3 +63,38 @@ def form_post(request: Request):
     result = ""
     afsusergroups = ['test', 'banana']
     return templates.TemplateResponse('index.html', context={'request': request, 'result': result, 'afsusergroups': afsusergroups})
+
+#stuff below is testing for the autofs existing entries copy
+class Option(BaseModel):
+    text: str
+    value: str
+
+source_data = {
+    "source1": [
+        {"text": "Option 1", "value": "1"},
+        {"text": "Option 2", "value": "2"},
+    ],
+    "source2": [
+        {"text": "Option 3", "value": "3"},
+        {"text": "Option 4", "value": "4"},
+    ],
+}
+
+destination_data = {
+    "destination1": [
+        {"text": "Option 5", "value": "5"},
+        {"text": "Option 6", "value": "6"},
+    ],
+    "destination2": [
+        {"text": "Option 7", "value": "7"},
+        {"text": "Option 8", "value": "8"},
+    ],
+}
+
+@app.get("/source/{source_id}", response_model=List[Option])
+async def get_source_options(source_id: str):
+    return source_data.get(source_id, [])
+
+@app.get("/destination/{destination_id}", response_model=List[Option])
+async def get_destination_options(destination_id: str):
+    return destination_data.get(destination_id, [])
