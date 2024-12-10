40d92bd2f265fab29b39099b3a812d085a18f2de
Tjaart van der Walt <git@tjaart.org>
2024-12-10 14:46:26 -0800

    Fix security group label (#33)

--- guam/models/secgroup.py
+++ guam/models/secgroup.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 
 
 class SecurityGroup(BaseModel):
-    groupname: str = Field(title="Username")
+    groupname: str = Field(title="Group Name")
     groupdesc: str = Field(title="Description")
